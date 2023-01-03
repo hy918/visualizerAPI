8ba3f0a3534ee574f08a9be8fc847ea32067a559
xielinrui <1721347156@qq.com>
2023-01-03 11:26:02 +0800

    :fire: 修改配置

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
 		'/api',
 		createProxyMiddleware({
 			// target: 'https://6170w159x5.goho.co',
-			target: 'http://127.0.0.1:8080',
+			target: 'http://127.0.0.1:9090',
 			// target: 'http://139.186.193.243:60001',
 			changeOrigin: true,
 		})
